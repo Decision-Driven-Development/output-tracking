8afe6de4b1959b3b8188fe386c68c588b29f35f1
Евгений Терехов <nergal@psu.ru>
2025-05-06 09:49:18 +0400

    feat: the ability to track output

---
+++ src/main/java/ewc/utilities/testableio/tracking/OutputListener.java
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Eugene Terekhov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package ewc.utilities.testableio.tracking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * I am responsible for keeping track of all the registered output trackers and
+ * for notifying them when an output is produced.
+ *
+ * @param <T> the type of the tracked output.
+ *
+ * @since 0.3
+ */
+public class OutputListener<T> {
+    private final List<OutputTracker<T>> trackers = Collections.synchronizedList(new ArrayList<>());
+
+    public List<OutputTracker<T>> trackers() {
+        return this.trackers;
+    }
+
+    public OutputTracker<T> trackOutput() {
+        final OutputTracker<T> tracker = new OutputTracker<>(this);
+        this.trackers.add(tracker);
+        return tracker;
+    }
+
+    public void stopTracking(OutputTracker<T> tracker) {
+        this.trackers.remove(tracker);
+    }
+
+    public void track(T output) {
+        this.trackers.forEach(tracker -> tracker.track(output));
+    }
+}
